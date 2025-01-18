7e7658e8db5f8cd3c2e64a7e5acde7947aa7d983
Adrian Winterstein <adrian@winterstein.biz>
2025-01-18 10:38:02 +0100

    application: enable logging module
    Enable the Zephyr logging module for the application build and add some
    log messages to the application main function.
    
    Signed-off-by: Adrian Winterstein <adrian@winterstein.biz>

--- application/src/main.cpp
+++ application/src/main.cpp
@@ -1,5 +1,7 @@
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(example, LOG_LEVEL_DBG);
 
 /* 1000 msec = 1 sec */
 #define SLEEP_TIME_MS 1000
@@ -15,6 +17,7 @@ static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 
 int main(void)
 {
+	LOG_DBG("Starting main function.");
 	int ret;
 
 	if (!gpio_is_ready_dt(&led)) {
@@ -27,6 +30,7 @@ int main(void)
 	}
 
 	while (1) {
+		LOG_INF("Toggling led.");
 		ret = gpio_pin_toggle_dt(&led);
 		if (ret < 0) {
 			return 0;
